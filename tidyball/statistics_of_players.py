4a529003196e272d3dc1a1fb005877e6c0ac4cc0
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-05-10 18:13:48 -0700

    ♻️ Use variable

--- tidyball/statistics_of_players.py
+++ tidyball/statistics_of_players.py
@@ -103,8 +103,9 @@ PASSES_NEW_NAMES = {
 
 
 def get_info_passes_by_player_from_data(data: dict) -> pd.DataFrame:
+    set_of_info = "passes"
     new_names = PASSES_NEW_NAMES
-    return get_info_by_player_from_data(data, "passes", new_names)
+    return get_info_by_player_from_data(data, set_of_info, new_names)
 
 
 def get_players(data: dict) -> list:
