4303680bc12de0c4966f2fa9bf2b6bd9e6a69b03
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-06-18 14:07:43 -0700

    ♻️ Extract `_assert_appearences_on_season_for()`

--- tests/test_stats_from_season_for_ahp.py
+++ tests/test_stats_from_season_for_ahp.py
@@ -7,7 +7,7 @@ berterame = read_json(data_to_test)
 
 
 def test_get_appearences_on_season_for_player():
-    expected_appearences = {
+    aguirre_appearences = {
         "appearences": 32,
         "lineups": 26,
         "minutes": 2257,
@@ -16,8 +16,7 @@ def test_get_appearences_on_season_for_player():
         "rating": "7.165625",
         "captain": False,
     }
-    obtained_appearences = get_appearences_on_season_for_player(aguirre)
-    assert expected_appearences == obtained_appearences
+    _assert_appearences_on_season_for(aguirre, aguirre_appearences)
     berterame_appearences = {
         "appearences": 37,
         "lineups": 37,
@@ -27,5 +26,9 @@ def test_get_appearences_on_season_for_player():
         "rating": "7.029729",
         "captain": False,
     }
-    obtained_appearences = get_appearences_on_season_for_player(berterame)
-    assert berterame_appearences == obtained_appearences
+    _assert_appearences_on_season_for(berterame, berterame_appearences)
+
+def _assert_appearences_on_season_for(player, expected_appearences):
+    obtained_appearences = get_appearences_on_season_for_player(player)
+    assert expected_appearences == obtained_appearences
+    
