b19f0a08e662e351ad9a3b6cb53d4ba611c1ac98
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-06-18 13:47:17 -0700

    🛑🧪 Test `get_appearences_on_season_for_player()`

--- tests/test_stats_from_season_for_ahp.py
+++ tests/test_stats_from_season_for_ahp.py
@@ -3,5 +3,16 @@ from tidyball import read_json, get_appearences_on_season_for_player
 data_to_test = "tests/data/data_file_16482_2021.json"
 jenssen = read_json(data_to_test)
 
+
 def test_get_appearences_on_season_for_player():
-    get_appearences_on_season_for_player()
+    expected_appearences = {
+        "appearences": 32,
+        "lineups": 26,
+        "minutes": 2257,
+        "number": None,
+        "position": "Attacker",
+        "rating": "7.165625",
+        "captain": False,
+    }
+    obtained_appearences = get_appearences_on_season_for_player()
+    assert expected_appearences == obtained_appearences
