c78163053bcd77917e4cebbf5256f19e45f16393
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-06-19 19:51:44 -0700

    ⬆️ Upgrade version

--- tidyball/__init__.py
+++ tidyball/__init__.py
@@ -3,7 +3,7 @@ Package to change the data of football downloaded from api-sport. The objetive
 is chage from format json to csv.
 """
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 from .io import *  # noqa
 from .statistics_of_players import *  # noqa
 from .stats_from_season_for_ahp import *  # noqa
